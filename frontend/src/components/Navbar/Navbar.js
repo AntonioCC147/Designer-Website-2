e8c76064688c1d82e01feac947eee69b20dfbaa1
AntonioCC147 <77618117+AntonioCC147@users.noreply.github.com>
2024-10-31 15:29:32 +0200

    Update

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -11,8 +11,10 @@ export default function NavbarComponent() {
     return (
         <Navbar sticky="top" expand="xxl" className="navbarComponent">
             <Container>
-                <Navbar.Brand href="/"><img src={Logo} className="logo" alt="Logo"/></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Brand href="/">
+                    <img src={Logo} className="logo" alt="Logo"/>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto" >
                     <Nav.Link href="/" className="navbarText">Home</Nav.Link>
@@ -22,7 +24,7 @@ export default function NavbarComponent() {
                     <Nav.Link href="#testimonials" className="navbarText">Testimonials</Nav.Link>
                     <Nav.Link href="#contact" className="navbarText">Contact Us</Nav.Link>
                     <div className="d-flex justify-content-center align-items-center">
-                    <Button className="navbarButton">Download CV</Button>
+                        <Button className="navbarButton" href="/cv.pdf">Download CV</Button>
                     </div>
                 </Nav>
                 </Navbar.Collapse>
